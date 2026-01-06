82f4ab4239f4b5818fc5c898055f464ba47fd941
zhaohaitang <zhaohaitang2001@gmail.com>
2026-01-06 19:18:05 +0800

    fix: 使用腾讯镜像并添加CI的Gradle缓存

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
 allprojects {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
         google()
         mavenCentral()
     }
